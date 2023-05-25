d119424098d1e49f6e5f8fa61aa7fdbb773f70c9
carbonbasedi <carbonbasedai@gmail.com>
2023-05-25 15:54:52 +0400

    init

---
+++ Task_PurpleBuzz/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Task_PurpleBuzz.Models;
+using Task_PurpleBuzz.ViewModels.About;
+
+namespace Task_PurpleBuzz.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var info = new List<Info>
+			{
+                new Info { Id = 1 , Title = "Our Vision" , Desc = "Incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse commodo viverra."},
+                new Info { Id = 2 , Title = "Our Mission" , Desc = "Eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam quis."},
+                new Info { Id = 3 , Title = "Our Goal" , Desc = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor."}
+            };
+
+            var model = new AboutIndexVM
+            {
+                Infos = info
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Task_PurpleBuzz/ViewModels/About/AboutIndexVM.cs
@@ -0,0 +1,9 @@
+﻿using Task_PurpleBuzz.Models;
+
+namespace Task_PurpleBuzz.ViewModels.About
+{
+	public class AboutIndexVM
+	{
+        public List<Info> Infos { get; set; }
+    }
+}
